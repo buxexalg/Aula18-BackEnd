01a7e55beff3d47922c2738bbc256b6680b9ffb6
Lucas Gabriel Romano <lucasgralcantara@gmail.com>
2020-08-22 11:04:37 -0300

    Correção: Otimização do código através de funções diferentes.

--- Aula18.js
+++ Aula18.js
@@ -1,21 +1,41 @@
 const fs = require("fs");
 
-const funcaoCallback = (err, data) => {
+const funcaoCallbackCartas = (err, data) => {
     if (err) {
         return console.log(err);
-    } 
-    const mensagensSeparadas = data.toString().split('---')
+    }
+    const mensagensSeparadas = constFormatarArquivo(data);
     for (i = 0; i < mensagensSeparadas.length; i++) {
-        let frasesSeparadas = mensagensSeparadas[i].split('\n')
-        frasesSeparadas.splice(frasesSeparadas.indexOf(''),1);
+        for(j=0; j < mensagensSeparadas[i].length; j++){
         console.log('[INICIO DA MENSAGEM]');
-        console.log('Remetente:', frasesSeparadas[0]);
-        console.log('Destinatário:', frasesSeparadas[1]);
-        console.log('Mensagem:', frasesSeparadas[2]);
+        console.log('Remetente:', mensagensSeparadas[i][0]);
+        console.log('Destinatário:', mensagensSeparadas[i][1]);
+        console.log('Mensagem:', mensagensSeparadas[i][2]);
         console.log('[FIM DA MENSAGEM]');
     }
-    
+    }
 }
 
-fs.readFile("cartas.txt", funcaoCallback);
+const separarMensagens = (a) => {
+    const mensagensSeparadas = a.toString().split('---');
+    return mensagensSeparadas;
+}
+
+const separarLinhas = (b) => {
+    linhasSeparadas = [];
+    for(i=0; i < b.length; i++) {
+        let linhas = b[i].split('\n');
+        linhas.splice(linhas.indexOf(''),1);
+        linhasSeparadas.push(linhas);
+    }
+    return linhasSeparadas
+}
+
+constFormatarArquivo = c => {
+    const arquivoSeparado = separarMensagens(c);
+    const arquivoFormatado = separarLinhas(arquivoSeparado);
+    return arquivoFormatado;
+}
+
+fs.readFile("cartas.txt", funcaoCallbackCartas);
 
